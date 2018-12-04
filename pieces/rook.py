a517ef9f0a5b60073a7ecda3a9b45e2b2ffcbb4c
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2018-12-04 23:51:48 +0100

    only legal moves for rook

--- pieces/rook.py
+++ pieces/rook.py
@@ -9,14 +9,41 @@ class Rook(Piece):
     def __repr__(self):
         return '{}R{}{}'.format(self.color, self.position_x, self.position_y)
 
-    def possible_moves(self):
-        return [
-            (x, self.position_y)
-            for x in range(8)
-        ] + [
-            (self.position_x, y)
-            for y in range(8)
-        ]
+    def possible_moves(self, board):
+        # TODO : To optimise by looping on delta_position
+        possible_moves = []
+        for x in range(self.position_x + 1, 8):
+            # increasing x moves
+            piece = board[x][self.position_y]
+            if piece:
+                if piece.color != self.color:
+                    possible_moves.append((x, self.position_y))
+                break
+            possible_moves.append((x, self.position_y))
+
+        for x in range(self.position_x - 1, -1, -1):
+            # decreasing x moves
+            piece = board[x][self.position_y]
+            if piece:
+                if piece.color != self.color:
+                    possible_moves.append((x, self.position_y))
+                break
+            possible_moves.append((x, self.position_y))
+
+        for y in range(self.position_y + 1, 8):
+            piece = board[self.position_x][y]
+            if piece:
+                if piece.color != self.color:
+                    possible_moves.append((self.position_x, y))
+                break
+            possible_moves.append((x, self.position_y))
+        for y in range(self.position_y - 1, -1, -1):
+            piece = board[self.position_x][y]
+            if piece and piece.color == self.color:
+                break
+            possible_moves.append((self.position_x, y))
+        return possible_moves
+
 
     def copy(self):
         return Rook(self.color, self.position_x, self.position_y)
