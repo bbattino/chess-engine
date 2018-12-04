b900c1aac1f97790d81b23b9b77b7a9c189c0c7d
bbattino <benjibattino@gmail.com>
2018-12-05 00:25:56 +0100

    add knight

--- pieces/piece.py
+++ pieces/piece.py
@@ -1,15 +1,20 @@
 class Piece:
 
-    def __init__(self, name, color, position_x, position_y):
-        self.name = name
+    def __init__(self, color, position_x, position_y):
         self.color = color
         self.position_x = position_x
         self.position_y = position_y
+
+    def __repr__(self):
+        return '{}?{}{}'.format(self.color, self.position_x, self.position_y)
 
     def set_position(self, x, y):
         self.position_x = x
         self.position_y = y
 
+    def possible_moves(self, board):
+        return []
 
-    def move(self):
-        ...
+
+    def copy(self):
+        return Piece(self.color, self.position_x, self.position_y)
