9ebe2a227980334176b51103243cb1d2627baaf3
bbattino <bbattino@enst.fr>
2018-12-05 19:06:18 +0100

    fix value method engine play

--- engine.py
+++ engine.py
@@ -18,17 +18,27 @@ class Engine:
             self.random_play()
         elif method == 'value':
             self.play_value()
+        elif method == 'human':
+            self.play_human()
 
     def random_play(self):
         self.board = random.choice(self.board.possible_move(self._white_to_play))
         self._white_to_play = 1 - self._white_to_play
 
     def play_value(self):
-        self.board = sorted(
-            self.board.possible_move(self._white_to_play), key=lambda b: - self._white_to_play * b.value()
-        )[0]
-        self._white_to_play = 1 - self._white_to_play
+        max_value = None
+        best_moves = []
+        all_moves = self.board.possible_move(self._white_to_play)
+        for index, move in enumerate(all_moves):
+            value = move.value() * (1 if self._white_to_play else -1)
+            if max_value is None or value > max_value:
+                max_value = value
+                best_moves = [move]
+            elif value == max_value:
+                best_moves.append(move)
 
+        self.board = random.choice(best_moves)
+        self._white_to_play = 1 - self._white_to_play
 
     def print_board(self):
         time.sleep(2)
