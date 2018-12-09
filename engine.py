6af27163b9bb391e7c88a7478ac33fc39b435a85
bbattino <benjibattino@gmail.com>
2018-12-09 11:52:19 +0100

    store possible moves

--- engine.py
+++ engine.py
@@ -4,31 +4,32 @@ from tools.printer import Printer
 import random
 import time
 
+
 class Engine:
     def __init__(self):
-        self.board = Board()
+        self.board = Board(method='exercice')
         self.printer = Printer()
         self._white_to_play = True
 
-    def play(self, method='value'):
+    def play(self, method='value', deapth=11):
         if method == 'random':
             self.random_play()
         elif method == 'value':
-            self.play_value()
-        elif method == 'human':
-            self.play_human()
+            self.play_value(deapth)
 
     def random_play(self):
         self.board = random.choice(self.board.possible_move(self._white_to_play))
         self._white_to_play = 1 - self._white_to_play
 
-    def play_value(self):
+    def play_value(self, depth):
         max_value = None
         best_moves = []
         coefficient = 1 if self._white_to_play else -1
         all_moves = self.board.possible_move(self._white_to_play)
+        if len(all_moves) == 0:
+            return 'PAT'
         for index, move in enumerate(all_moves):
-            value = move.evaluate(3, not self._white_to_play)
+            value = self.evaluate(move, depth, not self._white_to_play)
             if max_value is None or (coefficient * value > coefficient * max_value):
                 max_value = value
                 best_moves = [move]
@@ -37,6 +38,25 @@ class Engine:
         print('board value '+str(max_value))
         self.board = random.choice(best_moves)
         self._white_to_play = 1 - self._white_to_play
+
+    def evaluate(self, board, depth, is_white_playing):
+        if depth == 0:
+            return board.value()
+        '''
+        all_evaluations = []
+        for b in board.possible_move(is_white_playing):
+
+            all_evaluations.append(self.evaluate(b, depth - 1, not is_white_playing))
+        '''
+        all_evaluations = [
+            self.evaluate(b, depth-1, not is_white_playing)
+            for b in board.possible_move(is_white_playing)
+        ]
+        if len(all_evaluations) == 0:
+            return 0
+        if is_white_playing:
+            return max(all_evaluations)
+        return min(all_evaluations)
 
     def print_board(self):
         self.printer.print_html(self.board)
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from engine import Engine
 import time
+'''
+from board import Board
+from tools.printer import Printer
+b = Board()
+index = 0
+p = Printer()
+all_moves = b.possible_move(True)
+for i, move in enumerate(all_moves):
+    p.print_html(move, 'template/{}.html'.format(i))
 
+print(len(all_moves))
+# for move1 in all_moves:
+    #moves1 = move1.possible_move(False)
+    #for move in moves1:
+    #   move2 = move.possible_move(True)
+    # index +=1
+'''
 engine = Engine()
 engine.print_board()
 
-for _ in range(30):
-    # time.sleep(2)
-    engine.play()
+for i in range(30):
+    d = max(3, 9-i)
+    t = time.time()
+    engine.play(deapth=d)
+    print(time.time() - t, flush=True)
     engine.print_board()
+
